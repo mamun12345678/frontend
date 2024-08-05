c1fd91f0fa3511f367286fcf56bb07e75477837b
Md Abdullah Al Mamun <o.md.abdullahalmamun@gmail.com>
2024-08-05 09:05:03 +0100

    Update package.json, App.js, and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar'; // Import the NavBar component
-import Footer from './components/Footer'; // Import the Footer component
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
 import Blog from './components/Blog';
 import Resume from './components/Resume';
 import About from './components/About';
 import ContactForm from './components/ContactForm';
-import Home from './components/Home'; // Import Home component
+import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap'; // Import Container from react-bootstrap
-import './App.css'; // Import custom CSS
+import { Container } from 'react-bootstrap';
+import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/frontend">
       <NavBar />
       <Container className="mt-5">
         <Routes>
-          <Route path="/" element={<Home />} /> {/* Home component */}
+          <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/resume" element={<Resume />} />

9e5eb5ed84f14f67d0559ea20664deb0af553718
Md Abdullah Al Mamun <o.md.abdullahalmamun@gmail.com>
2024-08-05 11:03:04 +0100

    Add CNAME file and fix deployment issues

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/frontend">
+    <Router basename="/">
       <NavBar />
       <Container className="mt-5">
         <Routes>
